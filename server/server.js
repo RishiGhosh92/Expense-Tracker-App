f4c27e9e6d1835d9135937487f820bb0b69dac9d
Rishi Ghosh <rishi.ghosh@samsung.com>
2018-11-18 18:53:16 +0530

    Setup production build and Server

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const Hapi = require("hapi");
+const publicPath = path.join(__dirname, "..", "public");
+const Inert = require("inert");
+const port = process.env.PORT || 3000;
+
+const server = Hapi.server({
+  port: port,
+  host: "localhost"
+});
+
+const init = async () => {
+  // register static content plugin
+  await server.register(Inert);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+  // index route / homepage
+  server.route({
+    method: "GET",
+    path: "/create",
+    handler: (req, res) => {
+      return res.file(path.join(publicPath, "index.html"));
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/help",
+    handler: (req, res) => {
+      return res.file(path.join(publicPath, "index.html"));
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/{params*}",
+    handler: {
+      directory: {
+        path: publicPath
+      }
+    }
+  });
+};
+
+init();
